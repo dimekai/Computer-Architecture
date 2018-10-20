10fe2aaa9e953248f2c090fc4f0618c1aa07d54f
JesusDiaz08 <jesus_kdm@hotmail.com>
2018-10-20 00:05:27 -0500

    Archivo de Registros completo

--- Codes in class/Archivo de Registros/TB_ArchivoDeRegistros.vhd
+++ Codes in class/Archivo de Registros/TB_ArchivoDeRegistros.vhd
@@ -1,188 +1,240 @@
--- |==== LIBRARIES =====|
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   10:17:16 10/18/2018
+-- Design Name:   
+-- Module Name:   /home/erick/Documentos/Software/Xilinx/ARCHIVO_REGISTROS/TB_ARCHIVO_REGISTROS.vhd
+-- Project Name:  ARCHIVO_REGISTROS
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: ARCHIVO_DE_REGISTROS
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
 LIBRARY IEEE;
-USE IEEE.STD_LOGIC_1164.ALL;		
-USE STD.TEXTIO.ALL;					-- Entrada de datos por archivo
-USE IEEE.STD_LOGIC_TEXTIO.ALL;
+LIBRARY STD;
+USE STD.TEXTIO.ALL;
+USE IEEE.std_logic_TEXTIO.ALL;	--PERMITE USAR STD_LOGIC 
 
-ENTITY TB_ArchivoDeRegistros IS
-end TB_ArchivoDeRegistros;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.std_logic_UNSIGNED.ALL;
+USE IEEE.std_logic_ARITH.ALL;
+USE IEEE.numeric_std.ALL;
+ 
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY TB_ARCHIVO_REGISTROS IS
+END TB_ARCHIVO_REGISTROS;
 
-ARCHITECTURE behaviour OF TB_ArchivoDeRegistros IS
-	
-	COMPONENT ArchivoDeRegistros
-	PORT(
-		--DATA_OUT : INOUT STD_LOGIC_VECTOR( 15 DOWNTO 0);
-		SHAMT	: IN STD_LOGIC_VECTOR( 3 DOWNTO 0);
-		DIR	: IN STD_LOGIC;
-		CLK	: IN STD_LOGIC;
-		WR	: IN STD_LOGIC;
-		
-		-- MEMORIA RAM DISTRIBUIDA DE 3 PUERTOS
-		ADDR_WR	: IN STD_LOGIC_VECTOR( 3 DOWNTO 0);
-		ADDR_RD1: IN STD_LOGIC_VECTOR( 3 DOWNTO 0);
-		ADDR_RD2: IN STD_LOGIC_VECTOR( 3 DOWNTO 0);
-		WD	: IN STD_LOGIC_VECTOR( 15 DOWNTO 0);
-		DINOUT1	: INOUT STD_LOGIC_VECTOR( 15 DOWNTO 0);
-		DOUT2	: OUT STD_LOGIC_VECTOR( 15 DOWNTO 0);
-		SHE	: IN STD_LOGIC;  -- SHIFT ENABLE
-		--DIN	: INOUT STD_LOGIC_VECTOR( 15 DOWNTO 0)
-	);
-	END COMPONENT;
-	
-	
-	--INPUTS
-	SIGNAL SHAMT := STD_LOGIC_VECTOR( 3 DOWNTO 0) : = (OTHERS => '0');
-	SIGNAL DIR := STD_LOGIC := '0';
-	SIGNAL CLK := STD_LOGIC := '0';
-	SIGNAL WR := STD_LOGIC := '0';
-	
-	SIGNAL ADDR_WR := STD_LOGIC_VECTOR( 3 DOWNTO 0) := (OTHERS => '0');
-	SIGNAL ADDR_RD1 := STD_LOGIC_VECTOR( 3 DOWNTO 0) := (OTHERS => '0');
-	SIGNAL ADDR_RD2 := STD_LOGIC_VECTOR( 3 DOWNTO 0) := (OTHERS => '0');
-	
-	SIGNAL WD := STD_LOGIC_VECTOR( 15 DOWNTO 0) := (OTHERS => '0');
-	SIGNAL SHE := STD_LOGIC := '0';
-	
+ARCHITECTURE behavior OF TB_ARCHIVO_REGISTROS IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT ARCHIVO_DE_REGISTROS
+    PORT(
+         SHAMT : IN  std_logic_vector(3 downto 0);
+         DIR : IN  std_logic;
+         CLK : IN  std_logic;
+         WR : IN  std_logic;
+         ADDR_WR : IN  std_logic_vector(3 downto 0);
+         ADDR_RD1 : IN  std_logic_vector(3 downto 0);
+         ADDR_RD2 : IN  std_logic_vector(3 downto 0);
+         WD : IN  std_logic_vector(15 downto 0);
+         DINOUT1 : INOUT  std_logic_vector(15 downto 0);
+         DOUT2 : OUT  std_logic_vector(15 downto 0);
+         SHE : IN  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal SHAMT : std_logic_vector(3 downto 0) := (others => '0');
+   signal DIR : std_logic := '0';
+   signal CLK : std_logic := '0';
+   signal WR : std_logic := '0';
+   signal ADDR_WR : std_logic_vector(3 downto 0) := (others => '0');
+   signal ADDR_RD1 : std_logic_vector(3 downto 0) := (others => '0');
+   signal ADDR_RD2 : std_logic_vector(3 downto 0) := (others => '0');
+   signal WD : std_logic_vector(15 downto 0) := (others => '0');
+   signal SHE : std_logic := '0';
+
 	--BiDirs
-	SIGNAL DATA_OUT	: STD_LOGIC_VECTOR( 15 DOWNTO 0);
-	SIGNAL DINOUT1 : STD_LOGIC_VECTOR( 15 DOWNTO 0);
-	SIGNAL DIN : STD_LOGIC_VECTOR( 15 DOWNTO 0);
+   signal DINOUT1 : std_logic_vector(15 downto 0);
+
+ 	--Outputs
+   signal DOUT2 : std_logic_vector(15 downto 0);
+
+   -- Clock period definitions
+   constant CLK_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: ARCHIVO_DE_REGISTROS PORT MAP (
+          SHAMT => SHAMT,
+          DIR => DIR,
+          CLK => CLK,
+          WR => WR,
+          ADDR_WR => ADDR_WR,
+          ADDR_RD1 => ADDR_RD1,
+          ADDR_RD2 => ADDR_RD2,
+          WD => WD,
+          DINOUT1 => DINOUT1,
+          DOUT2 => DOUT2,
+          SHE => SHE
+        );
+
+   -- Clock process definitions
+   CLK_process :process
+   begin
+		CLK <= '0';
+		wait for CLK_period/2;
+		CLK <= '1';
+		wait for CLK_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+	--Archivo con los vectores de prueba
+	FILE ARCH_VEC : TEXT;
+	--Aquí almacenamos cada línea del archivo
+	VARIABLE LINEA_VEC : LINE;
+	-----------	VALORES DE ENTRADA ----------
+	--READ_REGISTER1
+	VARIABLE VAR_ADDR_RD1 : INTEGER;
+	--READ_REGISTER2
+	VARIABLE VAR_ADDR_RD2 : INTEGER;
+	--SHAMT
+	VARIABLE VAR_SHAMT : INTEGER;
+	--WRITE_REGISTER
+	VARIABLE VAR_ADDR_WR : INTEGER;
+	--WRITE DATA
+	VARIABLE VAR_WD : STD_LOGIC_VECTOR (WD'RANGE);
+	--WE
+	VARIABLE VAR_WR : STD_LOGIC;
+	--SHE
+	VARIABLE VAR_SHE : STD_LOGIC;
+	--DIR
+	VARIABLE VAR_DIR : STD_LOGIC;
 	
-	--OUTPUTS
-	SIGNAL DOUT2 : STD_LOGIC_VECTOR( 15 DOWNTO 0);
+	--Archivo donde escribiremos el resultado
+	FILE ARCH_RES: TEXT;
+	--Aquí almacenamos cada línea que vamos a escribir sobre el archivo
+	VARIABLE LINEA_RES : LINE;
+	-----------	VALORES DE SALIDA ----------
+	VARIABLE VAR_DOUT1 : STD_LOGIC_VECTOR (DINOUT1'RANGE);
+	VARIABLE VAR_DOUT2 : STD_LOGIC_VECTOR (DOUT2'RANGE);
 	
-	--CLOCK : Se debe definir el tiempo por cada ciclo
-	CONSTANT CLK_period : TIME := 10ns;
+	VARIABLE CADENA : STRING (1 TO 5);
+   begin		
+		--Abrimos el archivo que contiene los vectores prueba
+		file_open(ARCH_VEC, "input.txt", read_mode);
+		--Abrimos el archivo que contendrá los resultados
+		file_open(ARCH_RES, "output.txt", write_mode);
+		--------- ENCABEZADO DE LA TABLA ---------
+		CADENA := "  RR1";
+		write(LINEA_RES, CADENA, right, CADENA'LENGTH + 1);
+		CADENA := "  RR2";
+		write(LINEA_RES, CADENA, right, CADENA'LENGTH + 1);
+		CADENA := "   SH";
+		write(LINEA_RES, CADENA, right, CADENA'LENGTH + 1);
+		CADENA := " WREG";
+		write(LINEA_RES, CADENA, right, CADENA'LENGTH + 1);
+		CADENA := "   WD";
+		write(LINEA_RES, CADENA, right, CADENA'LENGTH + 1);
+		CADENA := "   WR";
+		write(LINEA_RES, CADENA, right, CADENA'LENGTH + 1);
+		CADENA := "  SHE";
+		write(LINEA_RES, CADENA, right, CADENA'LENGTH + 1);
+		CADENA := "  DIR";
+		write(LINEA_RES, CADENA, right, CADENA'LENGTH + 1);
+		CADENA := "  RD1";
+		write(LINEA_RES, CADENA, right, CADENA'LENGTH + 1);
+		CADENA := "  RD2";
+		write(LINEA_RES, CADENA, right, CADENA'LENGTH + 1);
+		-- AÑADIMOS EL ENCABEZADO AL ARCHIVO
+		writeline(ARCH_RES, LINEA_RES);
+		
+		WAIT FOR 100 NS;
+		-- INICIAMOS LA LECTURA DEL ARCHIVO CON LOS VECTORES PRUEBA
+		WHILE NOT endfile(ARCH_VEC) LOOP
+			--LEEMOS UNA LÍNEA COMPLETA DEL ARCHIVO
+			readline(ARCH_VEC, LINEA_VEC);
+			--Extraemos los valores dentro del archivo 
+			--EXTRAEMOS LO QUE EXISTE EN LA COLUMNA READ_REGISTER1
+			read(LINEA_VEC, VAR_ADDR_RD1);
+			ADDR_RD1 <= std_logic_vector(to_unsigned(VAR_ADDR_RD1, ADDR_RD1'LENGTH));
+			--EXTRAEMOS LO QUE EXISTE EN LA COLUMNA READ_REGISTER2
+			read(LINEA_VEC, VAR_ADDR_RD2);
+			ADDR_RD2 <= std_logic_vector(to_unsigned(VAR_ADDR_RD2, ADDR_RD2'LENGTH));
+			--EXTRAEMOS LO QUE EXISTE EN LA COLUMNA SHAMT
+			read(LINEA_VEC, VAR_SHAMT);
+			SHAMT <= std_logic_vector(to_unsigned(VAR_SHAMT, SHAMT'LENGTH));
+			--EXTRAEMOS LO QUE EXISTE EN LA COLUMNA WRITE_REGISTER
+			read(LINEA_VEC, VAR_ADDR_WR);
+			ADDR_WR <= std_logic_vector(to_unsigned(VAR_ADDR_WR, ADDR_WR'LENGTH));
+			--EXTRAEMOS LO QUE EXISTE EN LA COLUMNA WRITE_DATA
+			hread(LINEA_VEC, VAR_WD);
+			WD <= VAR_WD;
+			--EXTRAEMOS LO QUE EXISTE EN LA COLUMNA WR
+			read(LINEA_VEC, VAR_WR);
+			WR <= VAR_WR;
+			--EXTRAEMOS LO QUE EXISTE EN LA COLUMNA SHE
+			read(LINEA_VEC, VAR_SHE);
+			SHE <= VAR_SHE;
+			--EXTRAEMOS LO QUE EXISTE EN LA COLUMNA DIR
+			read(LINEA_VEC, VAR_DIR);
+			DIR <= VAR_DIR;
+		
+			WAIT UNTIL RISING_EDGE(CLK); --ESPERAMOS AL FLANCO DE SUBIDA
+--			VAR_DOUT1 := DINOUT1;
+--			VAR_DOUT2 := DOUT2;	
+			-- Escribimos el resultado
+			--ESCRIBE RR1
+			hwrite(LINEA_RES, ADDR_RD1, right, 6);
+			--ESCRIBE RR2
+			hwrite(LINEA_RES, ADDR_RD2, right, 6);
+			--ESCRIBE SH
+			hwrite(LINEA_RES, SHAMT, right, 6);
+			--ESCRIBE WREG
+			hwrite(LINEA_RES, ADDR_WR, right, 6);
+			--ESCRIBE WD
+			hwrite(LINEA_RES, WD, right, 6);
+			--ESCRIBE WR
+			write(LINEA_RES, WR, right, 6);
+			--ESCRIBE SHE
+			write(LINEA_RES, SHE, right, 6);
+			--ESCRIBE DIR
+			write(LINEA_RES, DIR, right, 6);
+			--ESCRIBE RD1
+			hwrite(LINEA_RES, DINOUT1, right, 6);
+			--ESCRIBE RD2
+			hwrite(LINEA_RES, DOUT2, right, 6);
+			WRITELINE(ARCH_RES, LINEA_RES);
+      END LOOP;
+		--CERRAMOS EL ARCHIVO CON LOS VECTORES PRUEBA
+		file_close(ARCH_VEC);
+		--CERRAMOS EL ARCHIVO CON LOS VECTORES RESULTANTES
+		file_close(ARCH_RES);
+		wait;
+   end process;
 
-	BEGIN
-
-		-- Instantiate the Unit Under Test (UUT)
-		UUT: ArchivoDeRegistros PORT MAP (
-			DATA_OUT => DATA_OUT,
-			SHAMT => SHAMT,
-			DIR => DIR,
-			CLK => CLK,
-			WR => WR,
-			ADDR_WR => ADDR_WR,
-			ADDR_RD1 => ADDR_RD1,
-			ADDR_RD2 => ADDR_RD2,
-			WD => WD,
-			DINOUT1 => DINOUT1,
-			DOUT2 => DOUT2,
-			SHE => SHE
-			--DIN => DIN
-		);
-
-		CLK_process : PROCESS
-		BEGIN
-			CLK <= '0';
-			WAIT FOR CLK_period/2;
-			CLK <= '1';
-			WAIT FOR CLK_period/2; 
-		END PROCESS;
-
-		STIM_PROC = PROCESS;
-		-- ====== ARCHIVOS Y VARIABLES DE ENTRADA ======
-		FILE ARCH_DATOS : TEXT,
-		VARIABLE LINEA : LINE;
-		VARIABLE VAR_READ_REGISTER1 : STD_LOGIC_VECTOR( 3 DOWNTO 0 );
-		VARIABLE VAR_READ_REGISTER2 : STD_LOGIC_VECTOR( 3 DOWNTO 0 );
-		VARIABLE VAR_SHAMT : STD_LOGIC_VECTOR( 3 DOWNTO 0 );
-		VARIABLE VAR_WRITE_REGISTER : STD_LOGIC_VECTOR( 3 DOWNTO 0 );
-		VARIABLE VAR_WRITE_DATA : STD_LOGIC_VECTOR( 15 DOWNTO 0 );
-		VARIABLE VAR_WR : STD_LOGIC;
-		VARIABLE VAR_SHE : STD_LOGIC;
-		VARIABLE VAR_DIR : STD_LOGIC;
-
-		-- ====== ARCHIVOS Y VARIABLES DE SALIDA ======
-		FILE ARCH_RES : TEXT;
-		VARIABLE LINEA_RES : LINE;
-		VARIABLE CADENA : STRING( 1 TO 5 );
-		VARIABLE ESPACIO : STRING( 1 TO 2 );
-		VARIABLE VAR_RD1 : STD_LOGIC_VECTOR( 15 DOWNTO 0 );
-		VARIABLE VAR_RD2 : STD_LOGIC_VECTOR( 15 DOWNTO 0 );
-
-		BEGIN
-			-- === ABRIENDO LOS ARCHIVOS ===
-			FILE_OPEN(ARCH_DATOS, "INPUT.TXT", READ_MODE);
-			FILE_OPEN(ARCH_RES, "RESULTADOS.TXT", WRITE_MODE);
-
-			-- ===== CABECERA DEL ARCHIVO ====
-			CADENA := "RR1  ";
-				WRITE(LINEA_RES, CADENA, RIGHT, CADENA'LENGTH);
-			CADENA := "RR2  ";
-				WRITE(LINEA_RES, CADENA, RIGHT, CADENA'LENGTH);
-			CADENA := "SH   ";
-				WRITE(LINEA_RES, CADENA, RIGHT, CADENA'LENGTH);
-			CADENA := "WD   ";
-				WRITE(LINEA_RES, CADENA, RIGHT, CADENA'LENGTH);
-			CADENA := "WR   ";
-				WRITE(LINEA_RES, CADENA, RIGHT, CADENA'LENGTH);
-			CADENA := "SHE  ";
-				WRITE(LINEA_RES, CADENA, RIGHT, CADENA'LENGTH);
-			CADENA := "DIR  ";
-				WRITE(LINEA_RES, CADENA, RIGHT, CADENA'LENGTH);
-			CADENA := "RD1  ";
-				WRITE(LINEA_RES, CADENA, RIGHT, CADENA'LENGTH);
-			CADENA := "RD2  ";
-				WRITE(LINEA_RES, CADENA, RIGHT, CADENA'LENGTH);
-
-			WRITELINE(ARCH_RES, LINEA_RES); -- SE ESCRIBE LA LINEA EN EL ARCHIVO
-
-			FOR I IN 0 TO 8 LOOP
-				READLINE(ARCH_DATOSM LINEA);
-				
-				HREAD(LINEA, VAR_READ_REGISTER1);
-				ADDR_RD1 <= VAR_READ_REGISTER1;
-
-				HREAD(LINEA, VAR_READ_REGISTER2);
-				ADDR_RD2 <= VAR_READ_REGISTER2;
-
-				HREAD(LINEA, VAR_SHAMT);
-				SHAMT <= VAR_SHAMT;
-
-				HREAD(LINEA, VAR_WRITE_REGISTER);
-				ADDR_RD1 <= VAR_WRITE_REGISTER;
-
-				HREAD(LINEA, VAR_WRITE_DATA);
-				WD <= VAR_WRITE_DATA;
-
-				READ(LINEA, VAR_WR);
-				WR <= VAR_WR;
-
-				READ(LINEA, VAR_SHE);
-				SHE <= VAR_SHE;
-
-				READ(LINEA, VAR_DIR);
-				DIR <= VAR_DIR;
-
-				WAIT UNTIL RISING_EDGE(CLK);
-
-				VAR_RD1 := DINOUT1;
-				VAR_RD2 := DOUT2;
-
-				HWRITE(LINEA_RES, VAR_READ_REGISTER1, RIGHT, 1);
-				HWRITE(LINEA_RES, VAR_READ_REGISTER2, RIGHT, 5);
-				HWRITE(LINEA_RES, VAR_SHAMT, RIGHT, 5);
-				HWRITE(LINEA_RES, VAR_WRITE_REGISTER, RIGHT, 5);
-				HWRITE(LINEA_RES, VAR_WRITE_DATA, RIGHT, 8);
-				WRITE(LINEA_RES, VAR_WR, RIGHT, 2);
-				WRITE(LINEA_RES, VAR_SHE, RIGHT, 5);
-				WRITE(LINEA_RES, VAR_DIR, RIGHT, 5);
-				HWRITE(LINEA_RES, VAR_RD1, RIGHT, 8);
-				HWRITE(LINEA_RES, VAR_RD2, RIGHT, 6);
-
-				WRITELINE(ARCH_RES, LINEA_RES);
-			
-			END LOOP;
-
-			FILE_CLOSE(ARCH_DATOS)
-			FILE_CLOSE(ARCH_RES);
-
-			WAIT;
-		END PROCESS;
-	END;
-	
-	
-	
-	
+END;
